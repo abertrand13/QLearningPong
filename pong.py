bacd91c32884164cf52ac5d14a16e8841b94f61f
Alex Bertrand <alex.bertrand13@gmail.com>
2015-12-21 11:44:29 -0500

    Removed old graphics.py references

--- pong.py
+++ pong.py
@@ -77,45 +77,6 @@ def main():
 		screen.blit(bg, (0,0)) # don't know why this is here either	
 		objects.draw(screen)
 		pygame.display.flip()
-
-		
-	
-	""" win = GraphWin("Pong", S_WIDTH, S_HEIGHT)
-	win.setBackground("#000000")
-
-	# create the wall (single player)	
-	wall = Rectangle(Point(10, 0), Point(10, S_HEIGHT))
-	wall.setOutline("#FFFFFF")
-	wall.draw(win)
-
-	# create the paddle
-	paddle = Rectangle(Point(S_WIDTH - 40 - P_WIDTH, S_WIDTH/2 - P_HEIGHT/2), Point(S_WIDTH - 40, S_WIDTH/2 + P_HEIGHT/2))
-	paddle.setFill("#FFFFFF")
-	paddle.setOutline("#FFFFFF")
-	paddle.draw(win)
-
-	# create the ball
-	ball = Rectangle(Point(S_WIDTH/2 - B_SIZE/2, S_HEIGHT/2 - B_SIZE/2), Point(S_WIDTH/2 + B_SIZE/2, S_HEIGHT/2 + B_SIZE/2))
-	ball.setFill("#FFFFFF")
-	ball.setOutline("#FFFFFF")
-	ball.draw(win)
-	
-	key = ''
-	balldx = B_SPEED
-	balldy = B_SPEED
-	while key != 'q':
-		# get the key being pressed	
-		key = win.getCurrKey()
-
-		# move paddle appropriately
-		if key == 'Up':
-			paddle.move(0, -P_SPEED)
-		elif key == 'Down':
-			paddle.move(0, P_SPEED)
-
-		# check collisions and move ball
-		# print(ball.getX())
-		ball.move(balldx, balldy)"""
 	
 	# input("press [ENTER] to exit")
 	win.close()
